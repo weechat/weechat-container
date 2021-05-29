9b2ab7fc69f2a172f754468e3808cf700b700527
Sébastien Helleu <flashcode@flashtux.org>
2021-05-29 21:31:15 +0200

    Format script with black

--- build.py
+++ build.py
@@ -22,28 +22,52 @@ import argparse
 import urllib.request
 import subprocess
 
-DISTROS = ('debian', 'alpine')
+DISTROS = (
+    "debian",
+    "alpine",
+)
 
 
 def get_parser():
     """Return the command line parser."""
-    parser = argparse.ArgumentParser(description='Build of WeeChat container')
-    parser.add_argument('-b', '--builder',
-                        default='docker',
-                        help=('program used to build the container image, '
-                              'like "docker" (default) or "podman"'))
-    parser.add_argument('-d', '--distro',
-                        choices=DISTROS,
-                        default=DISTROS[0],
-                        help='base Linux distribution for the container')
-    parser.add_argument('-n', '--dry-run', action='store_true',
-                        help=('dry run: display command but do not run it'))
-    parser.add_argument('-s', '--slim', action='store_true',
-                        help=('slim version: build without plugins: '
-                              'script, scripts, spell'))
-    parser.add_argument('version',
-                        help=('version to build: numeric (like "3.0") or '
-                              'alias: "latest" (or "stable"), "devel"'))
+    parser = argparse.ArgumentParser(description="Build of WeeChat container")
+    parser.add_argument(
+        "-b",
+        "--builder",
+        default="docker",
+        help=(
+            "program used to build the container image, "
+            "like docker (default) or podman"
+        ),
+    )
+    parser.add_argument(
+        "-d",
+        "--distro",
+        choices=DISTROS,
+        default=DISTROS[0],
+        help="base Linux distribution for the container",
+    )
+    parser.add_argument(
+        "-n",
+        "--dry-run",
+        action="store_true",
+        help=("dry run: display command but do not run it"),
+    )
+    parser.add_argument(
+        "-s",
+        "--slim",
+        action="store_true",
+        help=(
+            "slim version: build without plugins: " "script, scripts, spell"
+        ),
+    )
+    parser.add_argument(
+        "version",
+        help=(
+            'version to build: numeric (like "3.0") or '
+            'alias: "latest" (or "stable"), "devel"'
+        ),
+    )
     return parser
 
 
@@ -57,25 +81,28 @@ def get_version_tags(version, distro, slim):
     :rtype: list
     :return: tuple (version, tags) where version if the version of WeeChat
         to build and tags is a list of tag arguments for command line,
-        for example: ['-t', 'weechat:3.0-alpine', '-t', 'weechat:3-alpine']
+        for example: ['-t', 'weechat:3.0-alpine',
+                      '-t', 'weechat/weechat:3.0-alpine',
+                      '-t', 'weechat:3-alpine',
+                      '-t', 'weechat/weechat:3-alpine']
     """
-    str_slim = '-slim' if slim else ''
-    if distro == 'debian':
-        suffixes = [str_slim, f'-{distro}{str_slim}']
+    str_slim = "-slim" if slim else ""
+    if distro == "debian":
+        suffixes = [str_slim, f"-{distro}{str_slim}"]
     else:
-        suffixes = [f'-{distro}{str_slim}']
+        suffixes = [f"-{distro}{str_slim}"]
     tags = []
-    if version == 'devel':
+    if version == "devel":
         tags.append(version)
     else:
-        if version in ('latest', 'stable'):
-            tags.append('latest')
-            url = 'https://weechat.org/dev/info/stable/'
+        if version in ("latest", "stable"):
+            tags.append("latest")
+            url = "https://weechat.org/dev/info/stable/"
             with urllib.request.urlopen(url) as response:
-                version = response.read().decode('utf-8').strip()
-            numbers = version.split('.')
+                version = response.read().decode("utf-8").strip()
+            numbers = version.split(".")
             for i in range(len(numbers)):
-                tags.append('.'.join(numbers[:i + 1]))
+                tags.append(".".join(numbers[: i + 1]))
         else:
             tags.append(version)
     tags_args = []
@@ -83,10 +110,10 @@ def get_version_tags(version, distro, slim):
         for suffix in suffixes:
             tags_args.extend(
                 [
-                    '-t',
-                    f'weechat:{tag}{suffix}',
-                    '-t',
-                    f'weechat/weechat:{tag}{suffix}',
+                    "-t",
+                    f"weechat:{tag}{suffix}",
+                    "-t",
+                    f"weechat/weechat:{tag}{suffix}",
                 ]
             )
     return (version, tags_args)
@@ -95,16 +122,18 @@ def get_version_tags(version, distro, slim):
 def main():
     """Main function."""
     args = get_parser().parse_args()
-    slim = ['--build-arg', 'SLIM=1'] if args.slim else []
+    slim = ["--build-arg", "SLIM=1"] if args.slim else []
     version, tags = get_version_tags(args.version, args.distro, args.slim)
     command = [
-        f'{args.builder}',
-        'build',
-        '-f', f'{args.distro}/Containerfile',
-        '--build-arg', f'VERSION={version}',
+        f"{args.builder}",
+        "build",
+        "-f",
+        f"{args.distro}/Containerfile",
+        "--build-arg",
+        f"VERSION={version}",
         *slim,
         *tags,
-        '.',
+        ".",
     ]
     print(f'Running: {" ".join(command)}')
     if not args.dry_run:
@@ -116,5 +145,5 @@ def main():
             print(exc)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
